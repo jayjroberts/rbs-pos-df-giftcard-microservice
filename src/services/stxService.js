40d13148a31d83e72a16c747398284409223d950
Valeria Molina Recinos <89023516+valeria-molina@users.noreply.github.com>
2023-06-20 12:52:25 -0400

    added total taxes function

--- src/services/stxService.js
+++ src/services/stxService.js
@@ -16,11 +16,68 @@ const CONSTANTS = require('../constants/constants');
 // utils
 const tlogUtils = require('../utils/tlogUtils');
 
+/**
+ * Calculate the tax totals for STX record
+ * @param {Array} tlogs an array containing all tlogs for this store
+ * @returns {Object} an object containing the tax totals for STX record
+ */
+function getTaxesPerStoreId(tlogs) {
+    let totalTaxes = {};
+
+    for (let tlog of tlogs) {
+        // iterate through total taxes
+        for (let tax of tlog.tlog.totalTaxes) {
+            // get the tax id and name
+            const taxId = tax.id;
+            const taxName = tax.name;
+            // aggregate totalTaxes.amount.amount
+            if (Object.keys(totalTaxes).includes(taxId)) {
+                totalTaxes[taxId].sumOfTax += tax.amount.amount;
+            } else {
+                // create a new tax entry in totalTaxes object
+                totalTaxes[taxId] = {
+                    name: taxName,
+                    sumOfTax: tax.amount.amount,
+                    taxCollected: 0,
+                    taxDiscounted: 0,
+                };
+            }
+            // aggregate tax collected if there is no tax exempt amount available
+            if (!tax.taxExempt) {
+                // iterate through items in tlog object
+                for (let item of tlog.tlog.items) {
+                    // iterate through itemTaxes
+                    for (let itemTax of item.itemTaxes) {
+                        if (itemTax.id === taxId) {
+                            // aggregate itemTaxes.amount.amount
+                            totalTaxes[taxId].taxCollected +=
+                                itemTax.amount.amount;
+                        }
+                    }
+                }
+            } else {
+                // aggregate taxExempt.exemptAmount.amount
+                totalTaxes[taxId].taxDiscounted +=
+                    tax.taxExempt.exemptAmount.amount;
+            }
+        }
+    }
+    return totalTaxes;
+}
+
+/**
+ * Find all the tlogs that match the given criteria
+ * @param {string} runType 'daily' or 'weekly'
+ * @returns {Array} an array containing all matching documents
+ */
 async function findStxTLogs(runType) {
     LOGGER.debug(`Entering into findStxTLogs()`);
     // set query and projection
     const query = {
         'tlog.transactionType': 'SALES',
+        'tlog.isVoided': false,
+        'tlog.isSuspended': false,
+        'tlog.isRecalled': false,
     };
 
     const projection = {
@@ -76,6 +133,12 @@ async function findStxTLogs(runType) {
     }
 }
 
+/**
+ * This function executes all the STX calculations to generate an
+ * extract and uploads a fil to Azure Storage
+ * @param {string} runType 'daily' or 'weekly'
+ * @returns {string} a string containing the generated extract
+ */
 async function runSTX(runType) {
     try {
         let response = '';
@@ -84,6 +147,12 @@ async function runSTX(runType) {
 
         // sort by store id
         const logsByStore = tlogUtils.extractLogsByStoreId(tlogs);
+
+        for (let storeId of Object.keys(logsByStore)) {
+            const totals = getTaxesPerStoreId(logsByStore[storeId]);
+            LOGGER.debug(`Store ${storeId}`);
+            LOGGER.debug(JSON.stringify(totals));
+        }
         return logsByStore;
     } catch (error) {
         LOGGER.error(`Error in runSTX() :: ${error}`);
