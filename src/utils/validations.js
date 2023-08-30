ed6adc139e8cc9b5bb04c02415cfb2a20e04f56f
Valeria Molina Recinos <89023516+valeria-molina@users.noreply.github.com>
2023-08-30 11:11:01 -0400

    updated shared files

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -4,21 +4,33 @@
  * @version 1.0.0
  */
 
-const { query } = require('express-validator');
+const { check } = require('express-validator');
 
 const CONSTANTS = require('../constants/constants');
 
 // validate query parameters for routes
-const queryValidator = [
-    query(CONSTANTS.PARAMS.RUN)
+const validator = [
+    check(CONSTANTS.PARAMS.RUN)
         .exists()
         .withMessage(CONSTANTS.ERROR_DESC.MISSING_FIELD)
         .isString()
         .withMessage(CONSTANTS.ERROR_DESC.INVALID_FIELD)
         .custom((value) => CONSTANTS.RUN.includes(value))
         .withMessage(CONSTANTS.ERROR_DESC.INVALID_RUN),
+    check(CONSTANTS.BODY.START_DATE)
+        .if(check(CONSTANTS.PARAMS.RUN).equals(CONSTANTS.PARAMS.ADHOC))
+        .isString()
+        .withMessage(CONSTANTS.ERROR_DESC.MISSING_START_DATE)
+        .isDate()
+        .withMessage(CONSTANTS.ERROR_DESC.INVALID_DATE),
+    check(CONSTANTS.BODY.END_DATE)
+        .if(check(CONSTANTS.PARAMS.RUN).equals(CONSTANTS.PARAMS.ADHOC))
+        .isString()
+        .withMessage(CONSTANTS.ERROR_DESC.MISSING_END_DATE)
+        .isDate()
+        .withMessage(CONSTANTS.ERROR_DESC.INVALID_DATE),
 ];
 
 module.exports = {
-    queryValidator,
+    queryValidator: validator,
 };
